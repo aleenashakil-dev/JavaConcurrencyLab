826e4fc292c22f631d637cb430057d5262b9ed0c
HP <HP@DESKTOP-D2DO33N>
2025-12-11 21:56:09 +0500

    Initial Project Structure commit.

---
+++ JavaConcurrencyLab/src/com/lab7/ConcurrencyApp.java
@@ -0,0 +1,16 @@
+package com.lab7;
+
+public class ConcurrencyApp {
+
+    // This is the main method where Java programs begin execution.
+    public static void main(String[] args) {
+        
+        // This line prints a simple message to the console.
+        System.out.println("Lab 7 Project Started.");
+        System.out.println("Ready for Initial Commit and Push.");
+
+        // We will add the multithreading code here later.
+    }
+}
+
+
